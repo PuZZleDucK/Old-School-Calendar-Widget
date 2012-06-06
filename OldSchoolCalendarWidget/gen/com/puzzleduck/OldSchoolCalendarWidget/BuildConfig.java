06923ece22098ca3ef384a97314e1b7730832d65
PuZZleDucK <PuZZleDucK@gmail.com>
2012-06-06 13:09:31 +1000

    resizable widgets, removed unused xml layout

---
+++ OldSchoolCalendarWidget/gen/com/puzzleduck/OldSchoolCalendarWidget/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.puzzleduck.OldSchoolCalendarWidget;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
